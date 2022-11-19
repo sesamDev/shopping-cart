2ad48f6a767d4d79611f13e11b3233256ba11f88
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-19 10:19:41 +0100

    Added boilerplate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Catalog from "./components/routes/Catalog";
+import Checkout from "./components/Checkout";
+import Contact from "./components/routes/Contact";
+import Home from "./components/routes/Home";
+import Nav from "./components/Nav";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Nav />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
