b719f8b998ad92f7d1d225c2ce67ebc1f4d1c245
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-19 11:34:10 +0100

    Added test to see if nav buttons load correct page

--- src/tests/Nav.test.js
+++ src/tests/Nav.test.js
@@ -1,7 +1,40 @@
-import React from "react";
+import "@testing-library/jest-dom";
 
-const Nav = () => {
-  return <div>Navbar</div>;
-};
+import { render, screen } from "@testing-library/react";
 
-export default Nav;
+import App from "../App";
+import userEvent from "@testing-library/user-event";
+
+describe("Does links work", () => {
+  it("home button loads home component", () => {
+    render(<App />);
+    const homeButton = screen.getByRole("link", { name: "Home" });
+
+    userEvent.click(homeButton);
+    expect(screen.getByTestId("home")).toBeInTheDocument();
+  });
+
+  it("catalog button loads catalog component", () => {
+    render(<App />);
+    const catalogButton = screen.getByRole("link", { name: "Catalog" });
+
+    userEvent.click(catalogButton);
+    expect(screen.getByTestId("catalog")).toBeInTheDocument();
+  });
+
+  it("contact button loads contact component", () => {
+    render(<App />);
+    const contactButton = screen.getByRole("link", { name: "Contact" });
+
+    userEvent.click(contactButton);
+    expect(screen.getByTestId("contact")).toBeInTheDocument();
+  });
+
+  it("checkout button loads checkout component", () => {
+    render(<App />);
+    const checkoutButton = screen.getByRole("link", { name: "Checkout" });
+
+    userEvent.click(checkoutButton);
+    expect(screen.getByTestId("checkout")).toBeInTheDocument();
+  });
+});
