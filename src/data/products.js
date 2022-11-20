1250812da0d5993d628c436da7496c3852e06a82
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-20 11:45:38 +0100

    added initial product items

---
+++ src/data/products.js
@@ -0,0 +1,24 @@
+import gpu from "../assets/images/gpu.webp";
+import keyboard from "../assets/images/keyboard.webp";
+import macbook from "../assets/images/macbook.webp";
+import processor from "../assets/images/processor.webp";
+export const products = {
+  category: {
+    apple: {
+      title: "Apple",
+      img: macbook,
+    },
+    processors: {
+      title: "CPU's",
+      img: processor,
+    },
+    graphicsCards: {
+      title: "GPU's",
+      img: gpu,
+    },
+    keyboards: {
+      title: "Keyboards",
+      img: keyboard,
+    },
+  },
+};
