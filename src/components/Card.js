1c6006990bce8a57c99ad898a5392871a3e45b07
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-20 14:38:58 +0100

    Renders price tag if available

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,16 @@ import "../styles/Card.css";
 
 import React from "react";
 
+const hasPrice = (price) => {
+  return price !== undefined;
+};
+
 const Card = (props) => {
   return (
     <div className="card">
       <img src={props.img} alt="Card" />
       <h3>{props.title}</h3>
+      {hasPrice(props.price) ? <p>{props.price + "$"}</p> : ""}
     </div>
   );
 };
