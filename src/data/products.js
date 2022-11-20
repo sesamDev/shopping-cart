523c3e608526a6bcd4fb092efd93fee4dbdc1600
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-20 13:32:30 +0100

    Added ID to Categories

--- src/data/products.js
+++ src/data/products.js
@@ -2,23 +2,29 @@ import gpu from "../assets/images/gpu.webp";
 import keyboard from "../assets/images/keyboard.webp";
 import macbook from "../assets/images/macbook.webp";
 import processor from "../assets/images/processor.webp";
+import uniqid from "uniqid";
+
 export const products = {
   category: {
     apple: {
       title: "Apple",
       img: macbook,
+      id: uniqid(),
     },
     processors: {
       title: "CPU's",
       img: processor,
+      id: uniqid(),
     },
     graphicsCards: {
       title: "GPU's",
       img: gpu,
+      id: uniqid(),
     },
     keyboards: {
       title: "Keyboards",
       img: keyboard,
+      id: uniqid(),
     },
   },
 };
