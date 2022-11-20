46d25ec06247e43c71f9510c7b2421b3790df8a1
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-20 14:38:36 +0100

    Added func to load all available products

--- src/components/routes/Catalog.js
+++ src/components/routes/Catalog.js
@@ -1,5 +1,6 @@
 import "../../styles/Catalog.css";
 
+import Card from "../Card";
 import ProductCategoriesList from "../ProductCategoriesList";
 import React from "react";
 import { products } from "../../data/products";
@@ -7,10 +8,25 @@ import { products } from "../../data/products";
 const handleCategoryClick = () => {
   return console.log("Category selected");
 };
+
+const appendAllProducts = (products) => {
+  const allItems = Object.keys(products.category).map((c) => {
+    return Object.keys(products.category[c].items).map((i) => {
+      return products.category[c].items[i];
+    });
+  });
+  return allItems.map((category) => {
+    return category.map((item) => {
+      return <Card img={item.img} title={item.title} price={item.price} key={item.title} />;
+    });
+  });
+};
+
 const Catalog = () => {
   return (
     <div data-testid="catalog" className="catalog">
       <ProductCategoriesList products={products} handleCategoryClick={handleCategoryClick} />
+      {appendAllProducts(products)}
     </div>
   );
 };
