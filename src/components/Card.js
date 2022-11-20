e5ab29ab0a63fd8fc45b2c6eb3ab8dcf8349a7ce
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-20 15:06:55 +0100

    Render different title style depending on route

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,23 @@
 import "../styles/Card.css";
 
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const hasPrice = (price) => {
   return price !== undefined;
 };
 
+const isInCatalog = (location) => {
+  return location === "/catalog";
+};
+
 const Card = (props) => {
+  const location = useLocation();
   return (
     <div className="card">
+      {isInCatalog(location.pathname)}
       <img src={props.img} alt="Card" />
-      <h3>{props.title}</h3>
+      {isInCatalog(location.pathname) ? <p>{props.title}</p> : <h3>{props.title}</h3>}
       {hasPrice(props.price) ? <p>{props.price + "$"}</p> : ""}
     </div>
   );
