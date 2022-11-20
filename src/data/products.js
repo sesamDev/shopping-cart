d921fe82db523ccb0fab4da3afd367fab532358b
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-20 13:42:33 +0100

    added apple products

--- src/data/products.js
+++ src/data/products.js
@@ -1,4 +1,7 @@
+import appleWatchUltra from "../assets/images/apple-watch-ultra.jpeg";
 import gpu from "../assets/images/gpu.webp";
+import ipadPro from "../assets/images/ipad-pro.webp";
+import iphone14 from "../assets/images/iphone14.webp";
 import keyboard from "../assets/images/keyboard.webp";
 import macbook from "../assets/images/macbook.webp";
 import processor from "../assets/images/processor.webp";
@@ -10,6 +13,28 @@ export const products = {
       title: "Apple",
       img: macbook,
       id: uniqid(),
+      items: {
+        iphone14: {
+          title: "Iphone 14",
+          img: iphone14,
+          price: 1200,
+        },
+        ipadPro: {
+          title: "iPad Pro",
+          img: ipadPro,
+          price: 1700,
+        },
+        appleWatchUltra: {
+          title: "Apple Watch Ultra",
+          img: appleWatchUltra,
+          price: 999,
+        },
+        macbookPro: {
+          title: "MacBook Pro",
+          img: macbook,
+          price: 2200,
+        },
+      },
     },
     processors: {
       title: "CPU's",
