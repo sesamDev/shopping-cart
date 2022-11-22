6aa8df3d55128960bcb52bbf26737fb807fbc668
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-22 22:25:49 +0100

    Added test for cart in checkout

--- src/tests/Checkout.test.js
+++ src/tests/Checkout.test.js
@@ -1,7 +1,48 @@
-import React from "react";
+import Checkout, { createRenderReadyCart } from "../components/routes/Checkout";
+import { render, screen } from "@testing-library/react";
 
-const Checkout = () => {
-  return <div>Checkout</div>;
-};
+describe("Shopping cart", () => {
+  it("Summarize shopping cart state", () => {
+    <Checkout />;
+    const cart = [
+      {
+        item: "phone",
+        price: 100,
+        quantity: 1,
+      },
+      {
+        item: "phone",
+        price: 100,
+        quantity: 1,
+      },
+      {
+        item: "tv",
+        price: 900,
+        quantity: 1,
+      },
+      {
+        item: "tv",
+        price: 900,
+        quantity: 1,
+      },
+      {
+        item: "monitor",
+        price: 200,
+        quantity: 1,
+      },
+    ];
 
-export default Checkout;
+    createRenderReadyCart(cart);
+
+    // expect(createRenderReadyCart(cart).length).toBe(3);
+    expect(createRenderReadyCart(cart)[0].item).toBe("phone");
+    expect(createRenderReadyCart(cart)[1].item).toBe("tv");
+    expect(createRenderReadyCart(cart)[2].item).toBe("monitor");
+    expect(createRenderReadyCart(cart)[0].price).toBe(100);
+    expect(createRenderReadyCart(cart)[1].price).toBe(900);
+    expect(createRenderReadyCart(cart)[2].price).toBe(200);
+    expect(createRenderReadyCart(cart)[0].quantity).toBe(2);
+    expect(createRenderReadyCart(cart)[1].quantity).toBe(2);
+    expect(createRenderReadyCart(cart)[2].quantity).toBe(1);
+  });
+});
