3750e5dab7ddfecc3507656595def8c0bfea430b
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-22 22:23:36 +0100

    Added func to prep cart for render

--- src/components/routes/Checkout.js
+++ src/components/routes/Checkout.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+export const createRenderReadyCart = (cart) => {
+  // Get the types of items in cart
+  let itemsArray = cart.map((item) => item.item);
+
+  // reduce to uniqe items
+  let uniqeItemsArray = itemsArray.reduce((acc, curr) => {
+    if (!acc.includes(curr)) {
+      acc.push(curr);
+    }
+    return acc;
+  }, []);
+
+  let summarizedCart = uniqeItemsArray.map((curr) => {
+    let nObj = {
+      item: curr,
+      price: 0,
+      quantity: 0,
+    };
+    for (const obj of cart) {
+      if (obj.item === curr) nObj.quantity++;
+    }
+    for (const obj of cart) {
+      if (obj.item === curr && nObj.price === 0) nObj.price = obj.price;
+    }
+
+    return nObj;
+  });
+
+  return summarizedCart;
+};
 
 const Checkout = (props) => {
-  return <div data-testid="checkout">Checkout</div>;
+  const [renderCart, setRenderCart] = useState([]);
+  useEffect(() => {
+    setRenderCart(createRenderReadyCart(props.cart));
+    return () => {
+      //
+    };
+  }, [props.cart]);
+  return (
+    <div data-testid="checkout">
+      Checkout
+      <div>{console.log(renderCart)}</div>
+    </div>
+  );
 };
 
 export default Checkout;
